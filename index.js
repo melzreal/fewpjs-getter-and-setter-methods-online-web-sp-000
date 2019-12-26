baf73c70058f74ade9f9bc599eddc15429e96e3b
Melissa Real <melissa.real@hotmail.com>
2019-12-26 14:36:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ class Circle {
   constructor(radius) {
     this.radius = radius;
   }
- 
-  set phrase(phrase) {
-    this._phrase = phrase;
+  get diameter() {
+    return this.radius * 2
   }
- 
-  get speak() {
-    return `${this.name} says ${this._phrase || 'squawk'}`;
+
+  get area() {
+    return pi * ( this.radius * this.radius )
+  }
+
+  get circumference() {
+    return ( this.radius * 2 ) * pi
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter / 2
+  }
+
+  set circumference(circumference) {
+    this.radius = circumference/( pi * 2 )
+  }
+
+  set area(area) {
+    this.radius = Math.sqrt( area / pi )
   }
 }
  
