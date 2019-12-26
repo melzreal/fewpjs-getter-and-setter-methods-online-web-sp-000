76985f7bef7242ac6b6701125aecafd680956a1e
Melissa Real <melissa.real@hotmail.com>
2019-12-26 14:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(name) {
+    this.name = name;
+  }
+ 
+  set phrase(phrase) {
+    this._phrase = phrase;
+  }
+ 
+  get speak() {
+    return `${this.name} says ${this._phrase || 'squawk'}`;
+  }
+}
+ 
